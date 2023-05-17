802c5c065a2c28e88d4350f6cce988987003bee2
Serhii Shabala <sergei181989@gmail.com>
2023-05-17 23:27:26 +0300

    :bug: Fixed accordion toggle functionals.

--- script/main.js
+++ script/main.js
@@ -2,12 +2,18 @@
 
 const items = document.querySelectorAll('.content__accordion_item');
 
-items.forEach((elem) => {
-  elem.addEventListener('click', () => {
+items.forEach((element) => {
+  element.addEventListener('click', () => {
+    const isActive = element.classList.contains('active');
+
     items.forEach((item) => {
       item.classList.remove('active');
     });
 
-    elem.classList.add('active');
+    if (isActive) {
+      element.classList.remove('active');
+    } else {
+      element.classList.add('active');
+    }
   });
 });
